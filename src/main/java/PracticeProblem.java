7206a21ddb8563bf49374aa6df700889e1cb6c2b
Peter-Zheng1 <343277125@tdsb.ca>
2026-03-04 16:31:00 +0000

    done

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,3 +1,12 @@
+/**
+	* File: 1.12 Classwork
+	* Author: Peter Zheng
+	* Date Created: March 4, 2026
+	* Date Last Modified: March 4, 2026
+	*/
+
+
+import java.util.Scanner;
 public class PracticeProblem {
 
 	public static void main(String args[]) {
@@ -9,18 +18,42 @@ public class PracticeProblem {
 
 	public static void q1() {
 		//Write question 1 code here
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Input an integer: ");
+		int num = input.nextInt();
+		num = num + 3;
+		System.out.println(num);
+
 	}
 
 	public static void q2() {
 		//Write question 2 code here
+		Scanner input = new Scanner(System.in);
+		System.out.print("Input a number: ");
+		String num = (input.nextLine() + "4");
+		double newnum = Double.parseDouble(num);
+		System.out.println(newnum + 2);
+		
+
 	}
 
 	public static void q3() {
 		//Write question 3 code here
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Input a radius: ");
+		double rad = input.nextDouble();
+		System.out.println(3.14 * (rad * rad));
 	}
 
 	public static void q4() {
 		//Write question 4 code here
+		Scanner input = new Scanner(System.in);
+		System.out.print("Input a number: ");
+		Double num = input.nextDouble();
+		num = (num*12);
+		System.out.println(Math.floor(num));
 	}
 
 }
